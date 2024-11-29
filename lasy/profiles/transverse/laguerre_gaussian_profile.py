76311e76bf1cffdf7df3f7305b1806bed722d250
Emily Archer <emily.archer@desy.de>
2024-11-29 09:33:55 +0100

    Update lasy/profiles/transverse/laguerre_gaussian_profile.py
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- lasy/profiles/transverse/laguerre_gaussian_profile.py
+++ lasy/profiles/transverse/laguerre_gaussian_profile.py
@@ -48,8 +48,8 @@ class LaguerreGaussianTransverseProfile(TransverseProfile):
         The waist of the laser pulse,
         i.e. :math:`w_{0}` in the above formula.
     p : int (dimensionless)
-        The order of hermite polynomial in the x direction
-        i.e. :math:`m` in the above formula
+        The order of Hermite polynomial in the x direction
+        i.e. :math:`m` in the above formula.
     m : int (dimensionless)
         The order of hermite polynomial in the y direction
         i.e. :math:`n` in the above formula
